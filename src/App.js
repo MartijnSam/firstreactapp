638fc19d139813db6a5313a88c885d7c6955e80c
MartijnSam <martijnsamuels@gmail.com>
2020-04-28 14:34:22 +0200

    added title using title component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import angel from './denizboss.gif';
+import Title from './components/Title'
 
 import './App.css';
 
 function App() {
   return (
+         
+    
     <div className="App">
+      <main>
+        <Title name='Deniz'/>
+      </main>
       <header className="App-header">
         <img src={angel} alt='angel faced girl'></img>
         <p>Hello you sexy bitch!</p>
---
+++ src/components/Title.js
@@ -0,0 +1,6 @@
+// src/components/Title.js
+import React from "react";
+
+export default function Title({name}) {
+  return <h1>Hi there {name}!</h1>;
+}
